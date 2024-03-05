d42fd061364970fff5d26cf86cbb6de287a94f6d
Anmol131 <138642257+Anmol131@users.noreply.github.com>
2024-03-05 10:31:56 +0545

    Week01 is commited

---
+++ Week01/HelloWorld.java
@@ -0,0 +1,6 @@
+//Task1
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ Week01/PersonalDetails.java
@@ -0,0 +1,14 @@
+//Task//
+public class PersonalDetails {
+    public static void main(String[] args) {
+        String name = "Anmol Bohara";
+        String address = "Anamnagar,Kathmandu";
+        int age = 20;
+        long phoneNumber = 9749316394l; 
+
+        System.out.println("Name: " + name);
+        System.out.println("Address: " + address);
+        System.out.println("Age: " + age);
+        System.out.println("Phone Number: " + phoneNumber);
+    }
+}
